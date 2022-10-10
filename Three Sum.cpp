7427cf1c689f37f74861375670e253a846794569
vishal singh <76548939+impetus007@users.noreply.github.com>
2022-10-10 17:04:54 +0530

    make code readable with the help of comments

--- Three Sum.cpp
+++ Three Sum.cpp
@@ -8,42 +8,35 @@
 
  int main() { 
 
-     int n; 
-
-     cin >> n; 
+     int n;  // create a variable
+ 
+     cin >> n;  // taking an input from user
 
   
 
-     int arr[n]; 
+     int arr[n];  // create an array with size n
 
      for (int i = 0; i < n; i++){ 
 
          cin >> arr[i]; 
 
-     } 
+     }  // taking an array input using for loop
 
-  
-
-     int currentSum = 0; 
-
-     int maxSum = INT_MIN; 
-
+     int currentSum = 0;   // declare a varaible with currentsum=0;
+     int maxSum = INT_MIN;  // declare a maxSum with Min value.
      for(int i = 0; i < n; i++) { 
-
          currentSum += arr[i]; 
-
          if(currentSum < 0) { 
-
              currentSum = 0; 
-
          } 
 
          maxSum = max(maxSum, currentSum); 
-
      } 
 
      cout << maxSum <<endl; 
 
      return 0; 
+  
+  
 
  }
