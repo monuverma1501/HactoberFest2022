de28e9ad22d5282bb78c5b15512e32dd4c75e0b0
Mohit Anand <Praveenanand801@gmail.com>
2022-10-22 13:51:05 -0700

    Modified Push Dominoes code

--- 838-Push-Dominoes.cpp
+++ 838-Push-Dominoes.cpp
@@ -1,50 +1,62 @@
 class Solution {
 public:
     string pushDominoes(string dominoes) {
-        vector<pair<char,int >> arr;
-        arr.push_back({'L',-1});
-        
-        for(int i=0;i<dominoes.size();i++){
-            if(dominoes[i]=='L' || dominoes[i]=='R'){
-                arr.push_back({dominoes[i],i});
+        int n = dominoes.size();
+        vector<int> left(n,0),right(n,0);
+        char prev ='.';
+        int count = 1;
+        for(int i=0;i<n;i++) {
+            if(dominoes[i]=='R'){
+                prev = 'R';
+                count = 1;
+                continue;
+            }
+            else if(dominoes[i]== 'L'){
+                prev='L';
+            }
+            if(prev == 'R' && dominoes[i]== '.') {
+                right[i] = count;
+                count++;
             }
         }
-        string res = dominoes;
-        arr.push_back({'R',dominoes.size()});
-        for(int i =0;i<arr.size()-1;i++){
-            if(arr[i].first==arr[i+1].first){
-                for(int k=arr[i].second+1;k<arr[i+1].second;k++){
-                    res[k]= arr[i].first;
-                }
+
+        prev = '.';
+        count = 1;
+        for(int i=n-1;i>=0;i--) {
+            if(dominoes[i]== 'L') {
+                prev ='L';
+                count =1;
+                continue;
             }
-             else{
-                    if(arr[i].first>arr[i+1].first){
-                        int temp= arr[i].second + arr[i+1].second;
-                        if(temp%2==0){
-                            // cout<<temp<<endl;
-                            temp /=2;
-                            for(int k = arr[i].second;k<temp;k++){
-                                res[k]='R';
-                            }
-                            res[temp] = '.';
-                 for(int k = temp+1;k<=arr[i+1].second;k++){
-                                res[k]='L';
-                            }
-                        }
-                        else{
-                            temp/=2;
-                         for(int k = arr[i].second;k<=temp;k++){
-                                res[k]='R';
-                            }
-                            // res[temp] = '.';
-                         for(int k = temp+1;k<=arr[i+1].second;k++){
-                                res[k]='L';
-                            }
-                        }
-                    }
-                }
+            else if(dominoes[i]== 'R') {
+                prev = 'R';
+            }
+            if(prev == 'L' && dominoes[i]== '.') {
+                left[i] = count;
+                count++;
+            }
         }
-        return res;
-//      
+        string finalans="";
+        for(int i=0;i<n;i++) {
+            if(left[i]== 0 && right[i]== 0) {
+                finalans +=dominoes[i];
+            }
+            else if(left[i]==0) {
+                finalans += 'R';
+            }
+            else if(right[i]==0) {
+                finalans += 'L';
+            }
+            else if(left[i]==right[i]) {
+                finalans += '.';
+            }
+            else if(left[i]>right[i]) {
+                finalans += 'R';
+            }
+            else {
+                finalans += "L";
+            }
+        }
+        return finalans;
     }
 };
